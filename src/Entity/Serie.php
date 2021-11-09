d903c69c50bde3724c66877196f4fbdfc2debf44
Dorian GRIMAUD <DGRIMAUD>
2021-11-09 09:31:03 +0100

    entite serie ok

---
+++ src/Entity/Serie.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Serie
+ *
+ * @ORM\Table(name="serie")
+ * @ORM\Entity
+ */
+class Serie
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255, nullable=false)
+     */
+    private $titre;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="resume", type="text", length=0, nullable=true)
+     */
+    private $resume;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="duree", type="time", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="premiereDiffusion", type="date", nullable=true)
+     */
+    private $premierediffusion;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(?string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeInterface
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?\DateTimeInterface $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getPremierediffusion(): ?\DateTimeInterface
+    {
+        return $this->premierediffusion;
+    }
+
+    public function setPremierediffusion(?\DateTimeInterface $premierediffusion): self
+    {
+        $this->premierediffusion = $premierediffusion;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+
+}
